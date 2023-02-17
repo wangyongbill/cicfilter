ff497f19be72814209b8e06e5a222c5ba4bd7945
wangyongbill <1163886206@qq.com>
2023-02-17 18:52:09 +0800

    cic算法修改

--- Func_cic.m
+++ Func_cic.m
@@ -1,32 +1,58 @@
 function out = Func_cic(signal,M,R,D)
-
 %%
+Len = length(signal);       % 输入数据长度
+MIntgrat = zeros(1,M);       % M级积分寄存器
+MDiff = zeros(1,M);
+IntgratOut = zeros(1,Len);  % 积分输出
+DiffOut = zeros(1,Len/R);   % 差分输出
+%% M级积分器
+for i = 1:Len
+    sumtemp = signal(i);
+    for j = 1:M
+        MIntgrat(j) = MIntgrat(j)+sumtemp;
+        sumtemp = MIntgrat(j);
+    end
+    IntgratOut(i) = sumtemp;
+end
 
-
-
-
+%% 梳状滤波器 实现R抽取与D差分
+flg = 0;    % 输出数据个数标记
+for i = 1:Len 
+    % 抽取
+    if mod(i,R)==1
+        difftemp = IntgratOut(i);
+        for j = 1:M           
+            difftemp2 = difftemp - MDiff(j);  % D = R差分时延等于抽取倍率
+            MDiff(j) = difftemp;
+            difftemp = difftemp2;
+        end
+        flg = flg+1;
+        DiffOut(flg) = difftemp;         
+    end
+end
+out = DiffOut;
 %%
 % N = length(signal);% 数据点数
-% 
+%
 % % 积分器级联
 % cic_sum = zeros(1,N);
-% 
+%
 % cic_sum(1) = signal(1);
 % for i = 2:N
 %     cic_sum(i) = cic_sum(i-1)+signal(i);
 % end
-% 
+%
 % % 下抽取
 % cic_down = cic_sum(1:R:end);
-% 
+%
 % % 差分级联
 % cic_diff = zeros(1,N/R);
-% 
+%
 % cic_diff(1) = 0;
-% for i = 2:N/R 
+% for i = 2:N/R
 %     cic_diff(i) = cic_down(i) - cic_down(i-D);
 % end
-% 
+%
 % out = cic_diff;
 %% %%
 % % 积分器级联
@@ -35,35 +61,35 @@ function out = Func_cic(signal,M,R,D)
 % cic_sum3 = zeros(1,N);
 % cic_sum4 = zeros(1,N);
 % cic_sum5 = zeros(1,N);
-% 
+%
 % cic_sum1(1) = signal(1);
 % for i = 2:N
 %     cic_sum1(i) = cic_sum1(i-1)+signal(i);
 % end
-% 
+%
 % cic_sum2(1) = cic_sum1(1);%signal(1);
 % for i = 2:N
 %     cic_sum2(i) = cic_sum2(i-1)+cic_sum1(i);
 % end
-% 
+%
 % cic_sum3(1) = cic_sum2(1);
 % for i = 2:N
 %     cic_sum3(i) = cic_sum3(i-1)+cic_sum2(i);
 % end
-% 
+%
 % cic_sum4(1) = cic_sum3(1);
 % for i = 2:N
 %     cic_sum4(i) = cic_sum4(i-1)+cic_sum3(i);
 % end
-% 
+%
 % cic_sum5(1) = cic_sum4(1);
 % for i = 2:N
 %     cic_sum5(i) = cic_sum5(i-1)+cic_sum4(i);
 % end
-% 
+%
 % % 下抽取
 % cic_down = cic_sum5(1:R:end);
-% 
+%
 % % 差分级联
 % cic_diff1 = zeros(1,N/R);
 % cic_diff2 = zeros(1,N/R);
@@ -75,27 +101,27 @@ function out = Func_cic(signal,M,R,D)
 % for i = D+1:N/R
 %     cic_diff1(i) = cic_down(i) - cic_down(i-D);
 % end
-% 
+%
 % cic_diff2(1) = 0;
 % for i = D+1:N/R
 %     cic_diff2(i) = cic_diff1(i) - cic_diff1(i-D);
 % end
-% 
+%
 % cic_diff3(1) = 0;
 % for i = D+1:N/R
 %     cic_diff3(i) = cic_diff2(i) - cic_diff2(i-D);
 % end
-% 
+%
 % cic_diff4(1) = 0;
 % for i = D+1:N/R
 %     cic_diff4(i) = cic_diff3(i) - cic_diff3(i-D);
 % end
-% 
+%
 % cic_diff5(1) = 0;
 % for i = D+1:N/R
 %     cic_diff5(i) = cic_diff4(i) - cic_diff4(i-D);
 % end
-% 
+%
 % out = cic_diff5;
 
 end
